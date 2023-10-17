23f959ec96c17ad2816c7f735aca7859911fbaac
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-10-17 01:31:41 -0700

    WIP Main Page

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -2,7 +2,63 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <div className='row'>
+        <h1>Mini Poodles</h1>
+        <h3>Ultimate companion for all families</h3>
+      </div>
+
+      <div className='row'>
+        <h2>WHY POODLE?</h2>
+        <div className="col">
+          <h4>Do you value creature comforts but yearn for an adventure companion? The poodle is for you!</h4>
+          <p>
+            I fell in love with poodles because they are like Swiss Army Knives of the dog world! They can do it all; in style and can fit anywhere. They are happy to spend the day at your feet while you work and jump to action for a hike or city stroll. After a day of paddleboarding or shopping at an art festival, the poodle is happy to snuggle with you on the couch. Most important, being with you, in the center of your life, makes a poodle happy
+          </p>
+          <button className='btn btn-primary btn-lg'>About Us</button>
+
+        </div>
+        <div className="col">
+          <img src="" alt="" />
+        </div>
+      </div>
+
+      <div className='row'>
+        <h2>OUR BREEDING PRINCIPLES</h2>
+        <div className="col">
+          <img src="" alt="" />
+          <h4>Looks and Health</h4>
+          <p>The companion poodle must have a balanced and sound body structure to remain active and healthy. We show our parents in UKC and test through OFA and genetic testing to produce healthy</p>
+        </div>
+        <div className="col">
+          <img src="" alt="" />
+          <h4>Brains</h4>
+          <p>Our puppies succeed as easily trained everyday companions to their family, medical alert service dogs, dog therapy, and various sports - Obedience, Rally-O, Nosewrok, Barnhunt, and Fly Ball.</p>
+        </div>
+        <div className="col">
+          <img src="" alt="" />
+          <h4>Upbringing</h4>
+          <p>Puppies raised with early socialization curriculums are provided with the foundation to become well-adjusted adults and influence puppies' communication with humans.</p>
+        </div>
+      </div>
+
+      <div className='row'>
+        <h2>DISCOVER OUR DOGS:</h2>
+        <div className="col">
+          <h3>PUPPIES</h3>
+          <img src="" alt="" />
+        </div>
+        <div className="col">
+          <h3>OUR DOGS</h3>
+          <img src="" alt="" />
+        </div>
+        <div className="col">
+          <h3>EXTENDED FAMILY</h3>
+          <img src="" alt="" />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
