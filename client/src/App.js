b0f98ba466f4807f061dc87a46858e8bbce8a482
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-10-19 03:27:43 -0700

    Removed Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./views/Main";
-import Puppies from "./views/Puppies";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App container-fluid">
       <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/puppies" element={<Puppies />} />
-      </Routes>
+      <Main />
     </div>
   );
 }
--- client/src/views/Puppies.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Puppies = () => {
-  return (
-    <div>Puppies</div>
-  )
-}
-
-export default Puppies
