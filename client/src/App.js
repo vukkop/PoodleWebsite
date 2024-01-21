1ed739397e343a595932bb2e97815f5d26e1abfe
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-20 17:03:03 -0800

    Fixed favicon path

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Main from "./views/Main";
 import NavBar from "./components/NavBar";
 import { Helmet } from "react-helmet";
+import favicon from "../public/favicon.ico";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         <meta property="og:url" content="https://www.puppypoodles.com/"></meta>
         <meta property="og:type" content="website"></meta>
         <meta property="og:site_name" content="Mini Poodles"></meta>
-        <meta property="og:image" content="%PUBLIC_URL%/favicon.ico"></meta>
+        <meta property="og:image" content={{ favicon }}></meta>
         <meta
           property="og:description"
           content="Cute Poodle puppies looking for new home"
