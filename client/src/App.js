0c88a374220a7ecc1df60a7c87421ad95a1e39c7
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-20 16:43:57 -0800

    Added Helmet React for Meta tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 import Main from "./views/Main";
 import NavBar from "./components/NavBar";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App container-fluid">
+      <Helmet>
+        <meta
+          name="description"
+          content="Poodle puppies looking for new home"
+        />
+        <meta
+          name="keywords"
+          content="Poodle, Poodles, Puppy, Puppies, Dog, Mini, Edmonton, Vancouver"
+        ></meta>
+        <meta name="author" content="Vukasin Koprivica"></meta>
+        <meta property="og:url" content="https://www.puppypoodles.com/"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:site_name" content="Mini Poodles"></meta>
+        <meta property="og:image" content="%PUBLIC_URL%/favicon.ico"></meta>
+        <meta
+          property="og:description"
+          content="Cute Poodle puppies looking for new home"
+        ></meta>
+      </Helmet>
       <NavBar></NavBar>
       <Main />
     </div>
