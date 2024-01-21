f277aad33af51344774fe564d50274026aaf2f28
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-21 11:22:25 -0800

    Fixing meta tags

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,67 @@ function App() {
   return (
     <div className="App container-fluid">
       <Helmet>
+        {/* Primary Meta Tags */}
+        <title>Cute Mini Poodle Puppies - PuppyPoodles.com</title>
         <meta
           name="description"
-          content="Poodle puppies looking for new home"
+          content="Find adorable Poodle puppies looking for new homes. Our Mini Poodles are playful and well-trained, perfect for families."
         />
+
+        {/* Keywords */}
         <meta
           name="keywords"
-          content="Poodle, Poodles, Puppy, Puppies, Dog, Mini, Edmonton, Vancouver"
-        ></meta>
-        <meta name="author" content="Vukasin Koprivica"></meta>
-        <meta property="og:url" content="https://www.puppypoodles.com/"></meta>
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:site_name" content="Mini Poodles"></meta>
-        <meta property="og:image" content={{ favicon }}></meta>
+          content="Poodle, Poodles, Puppy, Puppies, Dog, M, Edmonton, Vancouver"
+        />
+
+        {/* Author */}
+        <meta name="author" content="Vukasin Koprivica" />
+
+        {/* Open Graph Meta Tags (Facebook, LinkedIn, etc.) */}
+        <meta
+          property="og:title"
+          content="Cute Mini Poodle Puppies - PuppyPoodles.com"
+        />
         <meta
           property="og:description"
-          content="Cute Poodle puppies looking for new home"
-        ></meta>
+          content="Find adorable Poodle puppies looking for new homes. Our Mini Poodles are playful and well-trained, perfect for families."
+        />
+        <meta property="og:image" content="%PUBLIC_URL%/favicon.ico" />
+        <meta property="og:url" content="https://www.puppypoodles.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Puppy Poodles" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="%PUBLIC_URL%/favicon.ico" />
+        <meta
+          name="twitter:title"
+          content="Cute Mini Poodle Puppies - PuppyPoodles.com"
+        />
+        <meta
+          name="twitter:description"
+          content="Find adorable Poodle puppies looking for new homes. Our Mini Poodles are playful and well-trained, perfect for families."
+        />
+        <meta name="twitter:image" content="%PUBLIC_URL%/favicon.ico" />
+
+        {/* Viewport Settings */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://www.puppypoodles.com/" />
+
+        {/* Robots Meta Tag */}
+        <meta name="robots" content="index, follow" />
+
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="Your Google Site Verification Code"
+        />
+
+        {/* Additional Custom Meta Tags */}
+        {/* Add any additional meta tags that are relevant to your content and SEO strategy */}
       </Helmet>
+
       <NavBar></NavBar>
       <Main />
     </div>
