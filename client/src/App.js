11f19827a721165ea2452a82028b8813813f67cb
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-22 14:58:22 -0800

    Fixed google search logo

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import "./App.css";
 import Main from "./views/Main";
 import NavBar from "./components/NavBar";
 import { Helmet } from "react-helmet";
-import LogoDark from "./assets/logo/logo.svg";
 
 function App() {
   return (
     <div className="App container-fluid">
       <Helmet>
         {/* Primary Meta Tags */}
-        <title>Mini Poodles</title>
+        <title>Mini Poodles - PuppyPoodles.com</title>
         <meta
           name="description"
           content="Find adorable Poodle puppies looking for new homes. Our Mini Poodles are playful and well-trained, perfect for families."
@@ -22,7 +21,7 @@ function App() {
         />
 
         {/* Author */}
-        <meta name="author" content="Vukasin Koprivica" />
+        <meta name="author" content="Vuk" />
 
         {/* Open Graph Meta Tags (Facebook, LinkedIn, etc.) */}
         <meta
@@ -33,13 +32,13 @@ function App() {
           property="og:description"
           content="Find adorable Poodle puppies looking for new homes. Our Mini Poodles are playful and well-trained, perfect for families."
         />
-        <meta property="og:image" content={{ LogoDark }} />
+        <meta property="og:image" content="./assets/logo/logo.svg" />
         <meta property="og:url" content="https://www.puppypoodles.com/" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Puppy Poodles" />
 
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content={{ LogoDark }} />
+        <meta name="twitter:card" content="./assets/logo/logo.svg" />
         <meta
           name="twitter:title"
           content="Cute Mini Poodle Puppies - PuppyPoodles.com"
@@ -48,7 +47,7 @@ function App() {
           name="twitter:description"
           content="Find adorable Poodle puppies looking for new homes. Our Mini Poodles are playful and well-trained, perfect for families."
         />
-        <meta name="twitter:image" content={{ LogoDark }} />
+        <meta name="twitter:image" content="./assets/logo/logo.svg" />
 
         {/* Viewport Settings */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
