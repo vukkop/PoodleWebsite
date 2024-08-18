fcb9cfb9dcfa9b7c75a114a03d837b6cdf712a20
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-08-18 11:53:24 -0700

    Updated Puppy

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -76,12 +76,18 @@ const Main = () => {
           </div>
 
           <div className="col-xl-3 col-md-6">
-            <div className="d-flex">
-              <img className='d-block rounded mx-auto img-fluid av-img' src={require('../assets/images/litter2/image1.1.jpeg')} alt="Puppie Girl 1" />
-              <img className='d-block rounded mx-auto img-fluid av-img' src={require('../assets/images/litter2/image1.2.jpeg')} alt="Puppie Girl 1" />
+            <div className="d-flex justify-content-center">
+              <div className='image d-flex justify-content-around w-100'>
+                <img className='d-block rounded mx-auto img-fluid av-img' src={require('../assets/images/litter2/image1.1.jpeg')} alt="Puppie Girl 1" />
+                <img className='d-block rounded mx-auto img-fluid av-img' src={require('../assets/images/litter2/image1.2.jpeg')} alt="Puppie Girl 1" />
+                <div className='just-rehomed'>
+                  <h5 className='px-2 py-1 m-0 text-light'>Just Rehomed</h5>
+                </div>
+              </div>
             </div>
-            <h4>Girl #1</h4>
+            <h4>Lily</h4>
           </div>
+
 
           <div className="col-xl-3 col-md-6">
             <div className="d-flex">
@@ -100,7 +106,6 @@ const Main = () => {
           </div>
 
           <div className="col-xl-3 col-md-6">
-
             <div className="d-flex justify-content-center">
               <div className='image d-flex justify-content-around w-100'>
                 <img className='d-block rounded mx-auto img-fluid av-img' src={require('../assets/images/litter2/image1.7.jpeg')} alt="Puppie Boy 4" />
